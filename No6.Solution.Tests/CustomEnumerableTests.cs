2046e6e864a8ef31182d7ce1a526eb3f4476e745
Valeria <valiretti@gmail.com>
2018-11-26 11:34:27 +0300

    Added task 6

--- No6.Solution.Tests/CustomEnumerableTests.cs
+++ No6.Solution.Tests/CustomEnumerableTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
 
 namespace No6.Solution.Tests
 {
@@ -8,25 +10,26 @@ namespace No6.Solution.Tests
         [Test]
         public void Generator_ForSequence1()
         {
-            int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
-
-            Assert.Inconclusive();
+            int[] expected = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+            var result = GenericGenerator.Generator(1, 1, (i, i1) => i + i1);
+            Assert.AreEqual(expected, result.Take(10));
         }
 
         [Test]
         public void Generator_ForSequence2()
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
-
-            Assert.Inconclusive();
+            var result = GenericGenerator.Generator(1, 2, (i, i1) => 6 * i1 - 8 * i);
+            Assert.AreEqual(expected, result.Take(10));
         }
 
         [Test]
+        [DefaultFloatingPointTolerance(0.000001)]
         public void Generator_ForSequence3()
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
-
-            Assert.Inconclusive();
+            var result = GenericGenerator.Generator<double>(1, 2, (i, i1) => i1 + i / i1).Take(10);
+            Assert.AreEqual(expected, result);
         }
     }
 }
---
+++ No6/GenericGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No6
+{
+    public static class GenericGenerator
+    {
+        public static IEnumerable<T> Generator<T>(T firstNumber, T secondNumber, Func<T, T, T> function)
+        {
+            yield return firstNumber;
+            yield return secondNumber;
+
+            T first = firstNumber;
+            T second = secondNumber;
+
+            while (true)
+            {
+                T next = function(first, second);
+                yield return next;
+
+                first = second;
+                second = next;
+            }
+        }
+    }
+}
